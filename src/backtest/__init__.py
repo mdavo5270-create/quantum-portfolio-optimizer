3a7c34f9fff8eee8155293f0617946426f1eceb2
mdavo5270-create <mdavo5270@gmail.com>
2026-08-15 22:29:17 +0100

    chore: exporter API backtest

--- src/backtest/__init__.py
+++ src/backtest/__init__.py
@@ -1,1 +1,17 @@
 """Moteur de backtesting historique."""
+
+from src.backtest.engine import (
+    BacktestMetrics,
+    BacktestResult,
+    compare_backtests,
+    compute_metrics,
+    run_backtest,
+)
+
+__all__ = [
+    "BacktestMetrics",
+    "BacktestResult",
+    "compare_backtests",
+    "compute_metrics",
+    "run_backtest",
+]
