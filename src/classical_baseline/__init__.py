a39d0d8bf5a17ccc011ad778b6d77f3e185c6953
mdavo5270-create <mdavo5270@gmail.com>
2026-08-15 21:49:38 +0100

    chore: exporter les fonctions publiques du module classical_baseline

--- src/classical_baseline/__init__.py
+++ src/classical_baseline/__init__.py
@@ -1,1 +1,17 @@
 """Optimisation classique (Markowitz / frontière efficiente) pour comparaison."""
+
+from src.classical_baseline.markowitz import (
+    OptimizationResult,
+    maximize_sharpe,
+    minimize_volatility,
+    optimize_from_returns,
+    portfolio_performance,
+)
+
+__all__ = [
+    "OptimizationResult",
+    "maximize_sharpe",
+    "minimize_volatility",
+    "optimize_from_returns",
+    "portfolio_performance",
+]
