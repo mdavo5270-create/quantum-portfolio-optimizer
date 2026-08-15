4fb2ce59d74bb1910f08d28b53e73d0d13b3dc5e
mdavo5270-create <mdavo5270@gmail.com>
2026-08-15 22:17:13 +0100

    chore: exporter l'API publique du module optimizer (SA)

--- src/optimizer/__init__.py
+++ src/optimizer/__init__.py
@@ -1,1 +1,15 @@
 """Cœur algorithmique quantum-inspired (recuit simulé, QAOA simulé, etc.)."""
+
+from src.optimizer.simulated_annealing import (
+    SAConfig,
+    compare_with_markowitz,
+    optimize_sa_from_returns,
+    simulated_annealing,
+)
+
+__all__ = [
+    "SAConfig",
+    "compare_with_markowitz",
+    "optimize_sa_from_returns",
+    "simulated_annealing",
+]
