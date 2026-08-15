9a955985e2b01ce963cddb61f897618f814de961
mdavo5270-create <mdavo5270@gmail.com>
2026-08-15 22:47:12 +0100

    chore: exporter cardinalité + QAOA

--- src/optimizer/__init__.py
+++ src/optimizer/__init__.py
@@ -1,5 +1,11 @@
 """Cœur algorithmique quantum-inspired (recuit simulé, QAOA simulé, etc.)."""
 
+from src.optimizer.cardinality import (
+    markowitz_cardinality,
+    optimize_cardinality_from_returns,
+    sa_cardinality,
+)
+from src.optimizer.qaoa_portfolio import QAOAConfig, optimize_qaoa_from_returns, qaoa_select_and_weight
 from src.optimizer.simulated_annealing import (
     SAConfig,
     compare_with_markowitz,
@@ -9,7 +15,13 @@ from src.optimizer.simulated_annealing import (
 
 __all__ = [
     "SAConfig",
+    "QAOAConfig",
     "compare_with_markowitz",
     "optimize_sa_from_returns",
     "simulated_annealing",
+    "markowitz_cardinality",
+    "sa_cardinality",
+    "optimize_cardinality_from_returns",
+    "optimize_qaoa_from_returns",
+    "qaoa_select_and_weight",
 ]
