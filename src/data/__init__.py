ab397ee5a63a6859a0b38616b394be7e4d32ffb9
mdavo5270-create <mdavo5270@gmail.com>
2026-08-15 21:49:40 +0100

    chore: exporter les fonctions publiques du module data

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,1 +1,5 @@
 """Récupération et nettoyage des données de marché."""
+
+from src.data.market_data import compute_returns, fetch_prices, prepare_data
+
+__all__ = ["compute_returns", "fetch_prices", "prepare_data"]
